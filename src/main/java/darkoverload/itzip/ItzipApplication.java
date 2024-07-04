8d384b75cdd7964ac40d2149d7aec5a605b268a4
hanseu9839 <hanseu9839@gmail.com>
2024-07-05 00:59:24 +0900

    first init project

---
+++ src/main/java/darkoverload/itzip/ItzipApplication.java
@@ -0,0 +1,13 @@
+package darkoverload.itzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItzipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/darkoverload/itzip/ItzipApplicationTests.java
@@ -0,0 +1,13 @@
+package darkoverload.itzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
