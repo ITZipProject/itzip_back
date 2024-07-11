f9993caf82aa3bfd5a2bd1869564d85bd3d98e87
김채민 <dktjdej25@gmail.com>
2024-07-11 16:15:34 +0900

    ⚙️Config: 기본 접근 권한 전체 허용으로 설정
    Related to: #4

---
+++ src/main/java/darkoverload/itzip/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package darkoverload.itzip.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+
+        return http.build();
+    }
+}
