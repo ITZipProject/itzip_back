77ae3fe289341aec744671d50a60357b89a5c2b8
김채민 <dktjdej25@gmail.com>
2024-07-12 10:38:29 +0900

    💡️docs: SecurityConfig 주석 추가
    Related to: #4

--- src/main/java/darkoverload/itzip/security/SecurityConfig.java
+++ src/main/java/darkoverload/itzip/security/SecurityConfig.java
@@ -7,15 +7,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+/**
+ * Spring Security 설정 클래스
+ * 애플리케이션의 보안 구성을 정의
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    /**
+     * 보안 필터 체인
+     *
+     * @param http 수정할 HttpSecurity 객체
+     * @return 구성된 SecurityFilterChain
+     * @throws Exception HttpSecurity 구성 시 발생한 예외
+     */
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // 모든 요청 허용
                 .authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                // cors 허용
                 .csrf(csrf -> csrf.disable());
 
+        // SecurityFilterChain을 빌드 후 반환
         return http.build();
     }
 }
