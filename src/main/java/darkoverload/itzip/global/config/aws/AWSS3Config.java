66d7a7681b14c5e0d29dc1d1d8c178fd9898e1a6
한승균 <hanseu9839@gmail.com>
2024-07-15 16:34:12 +0900

    ⚙️ config : aws s3 bucket 설정 추가
    - aws bucket 설정 추가

---
+++ src/main/java/darkoverload/itzip/global/config/aws/AWSS3Config.java
@@ -0,0 +1,36 @@
+package darkoverload.itzip.global.config.aws;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * S3 연결을 위한 Bean 등록
+ */
+@Configuration
+public class AWSS3Config {
+    @Value("${cloud.aws.credentials.accessKey")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+}
