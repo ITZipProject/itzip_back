7ade6728112f0aba6000266bcf5cff82a453c249
김채민 <dktjdej25@gmail.com>
2024-07-19 16:34:05 +0900

    ♻️ refactor: 예외 핸들링 규칙에 따른 수정
    예외 핸들링 규칙에 다른 커스텀 예외처리 클래스 삭제 및 리팩토링

--- src/main/java/darkoverload/itzip/global/config/response/handler/GlobalExceptionHandler.java
+++ src/main/java/darkoverload/itzip/global/config/response/handler/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package darkoverload.itzip.global.config.response.handler;
 import darkoverload.itzip.global.config.response.exception.RestApiException;
 import darkoverload.itzip.global.config.response.code.CommonExceptionCode;
 import darkoverload.itzip.global.config.response.handler.Util.ExceptionHandlerUtil;
-import darkoverload.itzip.jwt.exception.CustomJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,13 +17,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(RestApiException.class)
     public ResponseEntity<Object> handleRestApiException(RestApiException e) {
         log.error("RestApiException: {}", e.getExceptionCode().getData(), e);
-        return ExceptionHandlerUtil.handleExceptionInternal(e.getExceptionCode());
-    }
-
-    // CustomJwtException 예외를 처리하는 핸들러
-    @ExceptionHandler(CustomJwtException.class)
-    public ResponseEntity<Object> handleCustomJwtException(RestApiException e) {
-        log.error("CustomJwtException: {}", e.getExceptionCode().getData(), e);
         return ExceptionHandlerUtil.handleExceptionInternal(e.getExceptionCode());
     }
 
