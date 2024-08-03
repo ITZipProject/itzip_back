51917babe0ef8ba1371ae731a7df068417551921
parkseokwon <parkswon1@naver.com>
2024-08-03 14:56:01 +0900

    🐛	버그 수정 : MongdoDB 병합 충돌 해결 어노테이션
    @ExcludeFromJpaRepositories 를 통해서 리파지토리를 제외할 수 있다.
    
    JPA탐색에서 제외되는 방식을 채택했다.
    
    나머지 리파지토리를 제외해야해서 어노테이션을 추가했다.

--- src/main/java/darkoverload/itzip/sample/Repository/SampleRedisRepository.java
+++ src/main/java/darkoverload/itzip/sample/Repository/SampleRedisRepository.java
@@ -1,8 +1,10 @@
 package darkoverload.itzip.sample.Repository;
 
+import darkoverload.itzip.global.config.mongo.ExcludeFromJpaRepositories;
 import darkoverload.itzip.sample.domain.SampleRedis;
 import org.springframework.data.repository.CrudRepository;
 
+@ExcludeFromJpaRepositories
 public interface SampleRedisRepository extends CrudRepository<SampleRedis, String> {
 
 }
