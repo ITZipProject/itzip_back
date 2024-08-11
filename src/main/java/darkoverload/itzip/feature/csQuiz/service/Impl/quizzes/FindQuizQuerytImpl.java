90788231c3ac5df50161229ccc065c759538167b
parkseokwon <parkswon1@naver.com>
2024-08-11 23:30:10 +0900

    🎨	코드의 구조/형태 개선 : 로직이 이해하기 힘들었는데 좀더 개선 및 주석 추가

--- src/main/java/darkoverload/itzip/feature/csQuiz/service/Impl/quizzes/FindQuizQuerytImpl.java
+++ src/main/java/darkoverload/itzip/feature/csQuiz/service/Impl/quizzes/FindQuizQuerytImpl.java
@@ -1,8 +1,9 @@
 package darkoverload.itzip.feature.csQuiz.service.Impl.quizzes;
 
+import darkoverload.itzip.feature.csQuiz.entity.QuizUserSolvedMapping;
 import darkoverload.itzip.feature.csQuiz.entity.SortBy;
 import darkoverload.itzip.feature.csQuiz.controller.response.QuizDetailResponse;
-import darkoverload.itzip.feature.csQuiz.entity.QuizEntity;
+import darkoverload.itzip.feature.csQuiz.entity.QuizDocument;
 import darkoverload.itzip.feature.csQuiz.repository.quiz.QuizRepository;
 import darkoverload.itzip.feature.csQuiz.repository.quizusersolvedmapping.QuizUserSolvedMappingRepository;
 import darkoverload.itzip.feature.csQuiz.util.QuizMapper;
@@ -12,7 +13,11 @@ import darkoverload.itzip.global.config.response.code.CommonExceptionCode;
 import darkoverload.itzip.global.config.response.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -31,58 +36,71 @@ public class FindQuizQuerytImpl implements FindQiuzQuery {
     //User객체를 받아올 userRepository;
     private final UserRepository userRepository;
 
+    // PageModel을 만들기위한 주입
+    private final PagedResourcesAssembler<QuizDetailResponse> pagedResourcesAssembler;
+
     /**
      * 주어진 필터와 정렬 기준, 사용자 정보를 기반으로 퀴즈 목록을 조회하는 메서드
      *
      * @param difficulty 퀴즈 난이도 (선택 사항)
      * @param categoryId 카테고리 ID (선택 사항)
-     * @param sortBy 정렬 기준 (OLDEST 또는 NEWEST)
-     * @param userId 사용자 ID (0이면 모든 사용자)
-     * @param solved 사용자가 푼 문제만 조회할지 여부
-     * @param page 페이지 번호 (기본값: 0)
-     * @param size 페이지 크기 (기본값: 10)
+     * @param sortBy     정렬 기준 (OLDEST 또는 NEWEST)
+     * @param userId     사용자 ID (0이면 모든 사용자)
+     * @param inUserSolved     사용자가 푼 문제만 조회할지 여부
+     * @param page       페이지 번호 (기본값: 0)
+     * @param size       페이지 크기 (기본값: 10)
      * @return 필터링되고 정렬된 퀴즈 목록
      */
     @Override
-    public Page<QuizDetailResponse> QuizzesByDifficultyAndCategoryIdAndUserId(
+    @Transactional
+    public PagedModel<EntityModel<QuizDetailResponse>> QuizzesByDifficultyAndCategoryIdAndUserId(
             Integer difficulty, Long categoryId,
-            SortBy sortBy, Long userId, boolean solved, int page, int size) {
+            SortBy sortBy, Long userId, boolean inUserSolved, int page, int size) {
         // 페이지 정보와 정렬 기준을 기반으로 Pageable 객체 생성
         Pageable pageable = PageRequest.of(page, size, getSort(sortBy));
 
-        // 사용자가 푼 문제의 ID 목록을 저장할 리스트 초기화
+        // 사용자가 푼 문제의 목록을 저장할 리스트 초기화
+        List<QuizUserSolvedMapping> solvedProblemsEntity = List.of();
+        // 사용자가 푼 문제의 Id값들만 저장할 리스트 초기화
         List<String> solvedProblemIds = new ArrayList<>();
 
-        if (userId != 0){
+        // 사용자가 있으면 사용자가 푼 문제를 가져옴
+        if (userId != null) {
             //사용자를 찾고 사용자가 없을시 사용자가 없음 예외 출력
             UserEntity userEntity = userRepository.findById(userId).orElse(null);
             if (userEntity == null) {
                 throw new RestApiException(CommonExceptionCode.NOT_FOUND_USER);
             }
-            //사용자가 푼문의 id값들을 list로 받아옴
-            solvedProblemIds = quizUserSolvedMappingRepository.findProblemIdsByUser(userEntity);
-        }
-
-        Page<QuizEntity> quizzes;
-        List<QuizDetailResponse> quizDetailDtos;
-
-        if (solved) {
-            //사용자가 풀지 않은 문제를 가져옴
-            quizzes = quizRepository.findByDifficultyAndCategoryAndUserSolved(difficulty, categoryId, solvedProblemIds, pageable);//
-            quizDetailDtos = quizzes.getContent().stream()
-                    .map(quizMapper::entitiestoResponse)
-                    .toList();
-        } else {
-            //사용자가 풀었는지에 관련이 없는 문제를 가져옴
-            quizzes = quizRepository.findByDifficultyAndCategory(difficulty, categoryId, pageable);
-            //사용자가 푼문제라면 푼 문제라는 것을 표시함
-            Set<String> setSovledProblemIds = new HashSet<>(solvedProblemIds);
-            quizDetailDtos = quizzes.getContent().stream()
-                    .map(quiz -> quizMapper.entitiestoResponse(quiz, setSovledProblemIds))
+            //사용자가 푼문제매핑 테이블 list로 받아옴
+            solvedProblemsEntity = quizUserSolvedMappingRepository.findAllByUser(userEntity);
+            // 사용자가 푼 문제의 problemId를 리스트로 추출
+            solvedProblemIds = solvedProblemsEntity.stream()
+                    .map(QuizUserSolvedMapping::getProblemId)
                     .toList();
         }
+        //사용자가 푼문제들을 HashSet으로 만들어서 빠른 검색을 할수 있게함 key는 problemid값
+        Set<QuizUserSolvedMapping> solvedProblemsSet = new HashSet<>(solvedProblemsEntity);
 
-        return new PageImpl<>(quizDetailDtos, pageable, quizzes.getTotalElements());
+        //page된 quiz들을 받아옴
+        Page<QuizDocument> quizzes = quizRepository.findByDifficultyAndCategoryAndUserSolved(
+                difficulty, categoryId, solvedProblemIds, pageable, inUserSolved);
+
+
+        //solved가 Ture면 사용자가 푼문제를 포함하니 푼문제인지 표시를 해줘야함 아닐경우 그냥 dto 변환
+        List<QuizDetailResponse> quizDetailDtos = quizzes.getContent().stream()
+                .map(quiz -> {
+                    if (inUserSolved) {
+                        return quizMapper.documentsToResponse(quiz, solvedProblemsSet);
+                    } else {
+                        return quizMapper.documentsToResponse(quiz);
+                    }
+                })
+                .toList();
+
+        Page<QuizDetailResponse> quizDetailPage = new PageImpl<>(quizDetailDtos, pageable, quizzes.getTotalElements());
+
+        // PagedModel 생성
+        return pagedResourcesAssembler.toModel(quizDetailPage);
     }
 
     /**
