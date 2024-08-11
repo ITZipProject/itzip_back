3c5995dbaba50a70c43cc13f20e72ef500a964d4
parkseokwon <parkswon1@naver.com>
2024-08-11 23:23:35 +0900

    ♻️	코드 리팩토링 : PageModel을 사용하도록 변경

--- src/main/java/darkoverload/itzip/feature/csQuiz/service/Impl/quizzes/FindQiuzQuery.java
+++ src/main/java/darkoverload/itzip/feature/csQuiz/service/Impl/quizzes/FindQiuzQuery.java
@@ -2,11 +2,12 @@ package darkoverload.itzip.feature.csQuiz.service.Impl.quizzes;
 
 import darkoverload.itzip.feature.csQuiz.entity.SortBy;
 import darkoverload.itzip.feature.csQuiz.controller.response.QuizDetailResponse;
-import org.springframework.data.domain.Page;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 
 //퀴즈 관련 로직을 담아둘 인터페이스
 public interface FindQiuzQuery {
-    Page<QuizDetailResponse> QuizzesByDifficultyAndCategoryIdAndUserId(
+    PagedModel<EntityModel<QuizDetailResponse>> QuizzesByDifficultyAndCategoryIdAndUserId(
             Integer difficulty, Long categoryId,
             SortBy sortBy, Long userId, boolean solved, int page, int size);
 }
