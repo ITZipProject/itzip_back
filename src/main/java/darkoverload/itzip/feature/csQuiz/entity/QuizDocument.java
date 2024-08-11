617372b1cd2ab5925a73925463a845703562c476
parkseokwon <parkswon1@naver.com>
2024-08-11 23:36:00 +0900

    🎨	코드의 구조/형태 개선 : field 매핑을 통해서 명확히 choice들이 연결되도록 변경

--- src/main/java/darkoverload/itzip/feature/csQuiz/entity/QuizDocument.java
+++ src/main/java/darkoverload/itzip/feature/csQuiz/entity/QuizDocument.java
@@ -1,39 +1,61 @@
 package darkoverload.itzip.feature.csQuiz.entity;
 
-import darkoverload.itzip.global.entity.AuditingFields;
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import org.springframework.data.annotation.Id;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
 //퀴즈의 모든 정보를 담아두는 엔티티
 @Getter
 @Builder
+@AllArgsConstructor
 @Document(collection = "quizzes")
-public class QuizEntity extends AuditingFields{
-    @Id
+public class QuizDocument {
     //문제의 id값
+    @Id
+    @Field("_id")
     private String id;
+
     //문제
+    @Field("questionText")
     private String questionText;
+
     //문제 난이도
+    @Field("difficulty")
     private Integer difficulty;
+
     //문제 카테고리 id
+    @Field("categoryId")
     private Long categoryId;
+
     //문제 유형 번호
+    @Field("category")
     private String category;
+
     //문제 정답 번호
+    @Field("answer")
     private Integer answer;
+
     //문제를 맞춘 사람들 수
+    @Field("acceptedUserCount")
     private Integer acceptedUserCount;
+
     //문제를 시도한 사람들 수
+    @Field("triedUserCount")
     private Integer triedUserCount;
+
     //문제가 받은 평가 점수
+    @Field("points")
     private Integer points;
+
     //문제를 만든 사용자의 Id
+    @Field("createUserId")
     private Long createUserId;
+
     //문제 선택지 리스트
     private List<QuizChoice> choices;
 }
