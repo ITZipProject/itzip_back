73eaa996efccdb58b443b43ffb4bb38c0c5ffcec
한승균 <hanseu9839@gmail.com>
2024-08-15 00:26:43 +0900

    🎨 코드의 구조/형태 개선 : 서비스 쿼리 코드 custom repository 코드 구조 개선 변경

--- src/main/java/darkoverload/itzip/feature/school/repository/SchoolRepository.java
+++ src/main/java/darkoverload/itzip/feature/school/repository/SchoolRepository.java
@@ -1,13 +1,14 @@
 package darkoverload.itzip.feature.school.repository;
 
 import darkoverload.itzip.feature.school.entity.SchoolDocument;
+import darkoverload.itzip.feature.school.repository.custom.CustomSchoolRepository;
 import darkoverload.itzip.global.config.querydsl.ExcludeFromJpaRepositories;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
 
 @ExcludeFromJpaRepositories
-public interface SchoolRepository extends ElasticsearchRepository<SchoolDocument, Long>{
+public interface SchoolRepository extends ElasticsearchRepository<SchoolDocument, Long>, CustomSchoolRepository {
    long count();
 
 }
---
+++ src/main/java/darkoverload/itzip/feature/school/repository/custom/CustomSchoolRepository.java
@@ -0,0 +1,7 @@
+package darkoverload.itzip.feature.school.repository.custom;
+
+import java.util.List;
+
+public interface CustomSchoolRepository {
+    List<String> searchSchool(String schoolName);
+}
