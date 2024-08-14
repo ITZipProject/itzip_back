62b6d6abf2235f755a515a38f998b3d5307c0fa6
CodeJesterND <dev.hyoseung@gmail.com>
2024-08-14 21:15:28 +0900

    ⚙️ 환경설정 : .gitignore 환경설정 수정
    - main 패키지와 tesy 패키지 환경변수 설정파일 추가
    
    Ref : #35

---
+++ src/main/java/darkoverload/itzip/global/config/properties/PropertyConfig.java
@@ -0,0 +1,12 @@
+package darkoverload.itzip.global.config.properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/env.properties")
+})
+public class PropertyConfig {
+}
