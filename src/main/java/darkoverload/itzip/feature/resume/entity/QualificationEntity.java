a1b7542a6de8d3c4dacb2bbe67ca616b95ae4d6c
한승균 <hanseu9839@gmail.com>
2024-08-21 17:01:34 +0900

    💩 똥산 코드 : 자격증 엔티티 추가 및 @Column어노테이션 제거

--- src/main/java/darkoverload/itzip/feature/resume/entity/QualificationEntity.java
+++ src/main/java/darkoverload/itzip/feature/resume/entity/QualificationEntity.java
@@ -1,5 +1,6 @@
 package darkoverload.itzip.feature.resume.entity;
 
+
 import darkoverload.itzip.global.entity.AuditingFields;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -7,34 +8,28 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name="achievements")
-public class AchievementEntity extends AuditingFields {
+@Table(name="qualifications")
+public class QualificationEntity extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="user_id", nullable = false, insertable = false, updatable = false)
-    private Long userId;
-
     @Column(name="resume_id", nullable = false, insertable = false, updatable = false)
     private Long resumeId;
-
-    @Column(length=50)
-    private String name;
 
     @Column(name="organization", length=100)
     private String organization;
 
-    @Column(name="achievement_date")
-    private LocalDateTime achievementDate;
+    @Column(length=50)
+    private String name;
 
-    private String content;
+    private int score;
 
+    @Column(length=5)
+    private String level;
 }
