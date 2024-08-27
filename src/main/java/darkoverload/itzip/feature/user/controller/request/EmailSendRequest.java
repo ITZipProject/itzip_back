39ad3f31740b37aa873c6d5f62ea5f750f82a852
김채민 <dktjdej25@gmail.com>
2024-08-28 08:26:28 +0900

    🛠️ 리팩토링 : 불필요한 request field 삭제
    Related to: #87

--- src/main/java/darkoverload/itzip/feature/user/controller/request/EmailSendRequest.java
+++ src/main/java/darkoverload/itzip/feature/user/controller/request/EmailSendRequest.java
@@ -4,19 +4,17 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 이메일 발송 dto
  */
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class EmailSendRequest {
     @NotEmpty(message = "이메일을 입력해주세요.")
     @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$",
             message = "올바르지 않은 이메일 형식입니다.")
     private String email;
-
-    private String subject;
-
-    private String text;
 }
