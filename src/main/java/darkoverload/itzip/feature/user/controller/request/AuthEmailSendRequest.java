b9459acf7d0baaa9d6b9ec2f41ceb95a0a85f1e6
김채민 <dktjdej25@gmail.com>
2024-08-28 14:33:50 +0900

    ✨ 새 기능: 이메일 중복 체크 기능 #85
    Resolves: #85

--- src/main/java/darkoverload/itzip/feature/user/controller/request/AuthEmailSendRequest.java
+++ src/main/java/darkoverload/itzip/feature/user/controller/request/AuthEmailSendRequest.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmailSendRequest {
+public class AuthEmailSendRequest {
     @NotEmpty(message = "이메일을 입력해주세요.")
     @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$",
             message = "올바르지 않은 이메일 형식입니다.")
