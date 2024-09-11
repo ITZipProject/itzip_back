cce6ddb26ca34861d1aba4c05fa6119a62475e4f
한승균 <hanseu9839@gmail.com>
2024-09-11 20:14:34 +0900

    💩 똥 싼 코드 : JobInfoSearchResponse  회사 이름, url추가

--- src/main/java/darkoverload/itzip/feature/job/controller/response/JobInfoSearchResponse.java
+++ src/main/java/darkoverload/itzip/feature/job/controller/response/JobInfoSearchResponse.java
@@ -33,7 +33,12 @@ public class JobInfoSearchResponse {
     private Long experienceMax;
 
     private Integer scrapCount;
-    public JobInfoSearchResponse(Long id, String title, String industryName, String locationName,String jobName, LocalDateTime expirationDate, String experienceName, Long experienceMin, Long experienceMax, Integer scrapCount) {
+
+    private String companyName;
+
+    private String url;
+
+    public JobInfoSearchResponse(Long id, String title, String industryName, String locationName,String jobName, LocalDateTime expirationDate, String experienceName, Long experienceMin, Long experienceMax, Integer scrapCount, String companyName, String url) {
         this.id = id;
         this.title = title;
         this.industryName = industryName;
@@ -44,6 +49,8 @@ public class JobInfoSearchResponse {
         this.experienceMin = experienceMin;
         this.experienceMax = experienceMax;
         this.scrapCount = scrapCount;
+        this.companyName = companyName;
+        this.url = url;
     }
 
 }
