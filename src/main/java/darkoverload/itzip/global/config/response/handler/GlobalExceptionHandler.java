3c14a79e60738ecd3d6e2f7680f88cd1588b2b2b
parkseokwon <parkswon1@naver.com>
2024-09-13 00:23:01 +0900

    🐛	버그 수정 : 404,405 를 잡는 controller와 503  405 추가

--- src/main/java/darkoverload/itzip/global/config/response/handler/GlobalExceptionHandler.java
+++ src/main/java/darkoverload/itzip/global/config/response/handler/GlobalExceptionHandler.java
@@ -4,12 +4,15 @@ import darkoverload.itzip.global.config.response.exception.RestApiException;
 import darkoverload.itzip.global.config.response.code.CommonExceptionCode;
 import darkoverload.itzip.global.config.response.handler.Util.ExceptionHandlerUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+import javax.naming.ServiceUnavailableException;
 
 /*
 예외가 발생할시 반환을 가로채서 예외를 형식에 맞게 출력시키는 헨들러
@@ -55,5 +58,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleValidationExceptions(Exception e) {
         log.error("{}: {}", e.getClass().getSimpleName(), e.getMessage(), e);
         return ExceptionHandlerUtil.handleExceptionInternal(CommonExceptionCode.FILED_ERROR);
+    }
+
+    // 요청 시간 초과 시 발생하는 예외를 처리하는 핸들러
+    @ExceptionHandler(ClientAbortException.class)
+    public ResponseEntity<Object> handleRequestTimeout(ClientAbortException e) {
+        log.error("ClientAbortException: {}", e.getMessage(), e);
+        return ExceptionHandlerUtil.handleExceptionInternal(CommonExceptionCode.REQUEST_TIMEOUT);
+    }
+
+    // 서버 과부하 유지보수 일 경우 발생하는 예외를 처리하는 핸들러
+    @ExceptionHandler(ServiceUnavailableException.class)
+    public ResponseEntity<Object> handleServiceUnavailable(ServiceUnavailableException e) {
+        log.error("ServiceUnavailableException: {}", e.getMessage(), e);
+        return ExceptionHandlerUtil.handleExceptionInternal(CommonExceptionCode.SERVICE_UNAVAILABLE);
     }
 }
