4605da55e6e136e0f4d968b3bb84e1253eb1c03d
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-22 02:35:11 +0900

    ✨ 새 기능 : Blog 리포지토리 및 커스텀 쿼리 기능 추가
    - BlogRepository 인터페이스 추가 : 기본적인 JpaRepository 기능과 커스텀 메서드 포함.
    
    - CustomBlogRepository 인터페이스 추가 : 블로그 ID 및 사용자 ID를 기반으로 공개된 블로그 조회 메서드 정의.
    
    - CustomBlogRepositoryImpl 구현 : QueryDSL을 사용하여 블로그 ID와 사용자 ID를 기준으로 공개된 블로그 조회 로직 추가.
    
    Ref : #127

---
+++ src/main/java/darkoverload/itzip/feature/techinfo/repository/blog/custom/CustomBlogRepositoryImpl.java
@@ -0,0 +1,43 @@
+package darkoverload.itzip.feature.techinfo.repository.blog.custom;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import darkoverload.itzip.feature.techinfo.model.entity.BlogEntity;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static darkoverload.itzip.feature.techinfo.model.QBlogEntity.blogEntity;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomBlogRepositoryImpl implements CustomBlogRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    // 블로그 ID와 공개 여부를 기준으로 BlogEntity 조회
+    public Optional<BlogEntity> findByIdAndIsPublic(Long id) {
+        BlogEntity result = queryFactory
+                .selectFrom(blogEntity)
+                .where(blogEntity.id.eq(id)
+                        .and(blogEntity.isPublic.isTrue())) // 공개된 블로그만 조회
+                .fetchOne();
+
+        return Optional.ofNullable(result); // 결과를 Optional로 반환
+    }
+
+    // 사용자 ID와 공개 여부를 기준으로 BlogEntity 조회
+    @Override
+    public Optional<BlogEntity> findByUserIdAndIsPublic(Long userId) {
+        BlogEntity result = queryFactory
+                .selectFrom(blogEntity)
+                .where(blogEntity.userId.eq(userId)
+                        .and(blogEntity.isPublic.isTrue())) // 공개된 블로그만 조회
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+}
