d2bb24ff53b284b5bfaeb7a0e484bc120239b1ed
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-22 02:37:43 +0900

    ✨ 새 기능 : Comment 리포지토리 및 커스텀 쿼리 기능 추가
    - CommentRepository 인터페이스 추가 : MongoRepository와 CustomCommentRepository를 통한 커스텀 메서드 포함.
    
    - CustomCommentRepository 인터페이스 추가 : 포스트 ID로 댓글 조회, 댓글 내용 및 공개 여부 업데이트 메서드 정의.
    
    - CustomCommentRepositoryImpl 구현 : MongoTemplate을 사용하여 댓글 조회, 댓글 내용 및 공개 여부 업데이트 로직 구현.
    
    - 페이지네이션을 통해 특정 포스트에 대한 댓글 목록을 조회하고, 댓글을 업데이트하거나 공개 여부를 변경하는 기능 추가.
    
    Ref : #127

---
+++ src/main/java/darkoverload/itzip/feature/techinfo/repository/comment/custom/CustomCommentRepository.java
@@ -0,0 +1,41 @@
+package darkoverload.itzip.feature.techinfo.repository.comment.custom;
+
+import darkoverload.itzip.feature.techinfo.model.document.CommentDocument;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * CommentDocument와 관련된 커스텀 데이터베이스 작업을 정의하는 인터페이스.
+ * 포스트 ID로 댓글 조회, 댓글 내용 업데이트, 댓글 공개 여부 변경 등의 메서드를 포함함.
+ */
+public interface CustomCommentRepository {
+
+    /**
+     * 주어진 포스트 ID에 해당하는 댓글 목록을 페이지네이션하여 조회.
+     *
+     * @param postId 포스트 ID
+     * @param pageable 페이지네이션 정보
+     * @return 페이지네이션된 댓글 목록
+     */
+    Page<CommentDocument> findCommentsByPostId(ObjectId postId, Pageable pageable);
+
+    /**
+     * 주어진 댓글 ID에 해당하는 댓글 내용을 업데이트.
+     *
+     * @param commentId 댓글 ID
+     * @param content 새로운 댓글 내용
+     * @return 업데이트 성공 여부
+     */
+    boolean updateComment(ObjectId commentId, String content);
+
+    /**
+     * 주어진 댓글 ID에 해당하는 댓글의 공개 여부를 변경.
+     *
+     * @param commentId 댓글 ID
+     * @param isPublic 공개 여부 (true: 공개, false: 비공개)
+     * @return 업데이트 성공 여부
+     */
+    boolean updateCommentVisibility(ObjectId commentId, boolean isPublic);
+}
