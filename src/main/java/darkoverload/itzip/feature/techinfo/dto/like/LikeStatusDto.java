f93ee538f6a001132fcb300017bd9f04cb28d7c8
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-22 02:51:04 +0900

    💡 주석 추가 : LikeStatusDto 클래스에 상세 주석 추가
    - 각 필드에 대한 설명 주석 추가 : 유저 ID, 포스트 ID, 좋아요 상태에 대한 설명 포함.
    
    - convertToDomain 메서드에 대한 설명 추가 : Like 도메인 객체로 변환하는 기능 설명.
    
    - 코드 가독성과 이해도를 높이기 위해 주석 추가.
    
    Ref : #128

--- src/main/java/darkoverload/itzip/feature/techinfo/dto/like/LikeStatusDto.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/dto/like/LikeStatusDto.java
@@ -6,15 +6,36 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * Like 상태를 나타내는 DTO (Data Transfer Object) 클래스.
+ * 특정 유저가 특정 포스트에 대해 좋아요를 눌렀는지 여부를 관리함.
+ */
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LikeStatusDto {
-    private Long userId;
-    private String postId;
-    private Boolean isLiked;  // true면 좋아요 추가, false면 좋아요 취소
 
+    /**
+     * 유저 ID (좋아요를 누른 유저).
+     */
+    private Long userId;
+
+    /**
+     * 포스트 ID (좋아요가 눌린 포스트).
+     */
+    private String postId;
+
+    /**
+     * 좋아요 상태 (true면 좋아요 추가, false면 좋아요 취소).
+     */
+    private Boolean isLiked;
+
+    /**
+     * LikeStatusDto 객체를 Like 도메인 객체로 변환.
+     *
+     * @return 변환된 Like 도메인 객체
+     */
     public Like convertToDomain() {
         return Like.builder()
                 .userId(this.userId)
