fe6d62609f57d91c945efddcedbd6c46d8d668b7
한승균 <hanseu9839@gmail.com>
2024-09-25 11:00:05 +0900

    🐞 ConnectService : Delete Scrap 추가

--- src/main/java/darkoverload/itzip/feature/job/repository/custom/CustomJobInfoScrapRepository.java
+++ src/main/java/darkoverload/itzip/feature/job/repository/custom/CustomJobInfoScrapRepository.java
@@ -2,8 +2,11 @@ package darkoverload.itzip.feature.job.repository.custom;
 
 import darkoverload.itzip.feature.job.entity.JobInfoScrapEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomJobInfoScrapRepository {
     Optional<JobInfoScrapEntity> findByJobInfoId(Long id,String email);
+
+    Long deleteDeleteByPositionIds(List<Long> positionIds);
 }
