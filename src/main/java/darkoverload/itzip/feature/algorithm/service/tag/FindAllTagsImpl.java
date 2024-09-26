f6888d0fd7fe39f633a951d64272b83c4de1d97b
parkseokwon <parkswon1@naver.com>
2024-09-26 22:21:18 +0900

    ♻️	코드 리팩토링 : 추천 tag 추가

--- src/main/java/darkoverload/itzip/feature/algorithm/service/tag/FindAllTagsImpl.java
+++ src/main/java/darkoverload/itzip/feature/algorithm/service/tag/FindAllTagsImpl.java
@@ -18,9 +18,9 @@ public class FindAllTagsImpl implements FindAllTags {
     private final ProblemTagRepository problemTagRepository;
 
     //프로그래머스 고득점 키트를 기준으로했음
-    //너비, 깊이, 이분탐색, 그리디, 그래프탐색, 다이나믹, 시뮬레이션, 백트래킹, 우선순위, 트리, 정렬, 스택, 큐
+    //너비, 깊이, 이분탐색, 그리디, 그래프탐색, 다이나믹, 시뮬레이션, 백트래킹, 우선순위, 트리, 정렬, 스택, 큐, 자료구조
     private final List<Long> recommendedTags = new ArrayList<>(Stream.of(
-            126, 127, 12, 33, 11, 25, 59, 141, 5, 120, 97, 71, 72
+            126, 127, 12, 33, 11, 25, 59, 141, 5, 120, 97, 71, 72 ,175
     ).map(Integer::longValue).toList());
 
     /**
