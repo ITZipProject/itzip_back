d29535c4eb85b839bdd0f9f4961e55606a3e302c
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-26 16:00:48 +0900

    🗃 데이터베이스 관련 수정 : blogs 테이블에 user_id를 Primary Key로 설정
    - blogs 테이블에서 user_id를 Primary Key로 설정하여 블로그 소유자의 회원 ID와 일치하도록 수정
    - 블로그 소개글 필드(intro) 추가 (최대 255자)

--- src/main/resources/sql/techinfo/techinfo_blogs.sql
+++ src/main/resources/sql/techinfo/techinfo_blogs.sql
@@ -1,8 +1,8 @@
 -- 테이블 생성: blogs
 CREATE TABLE blogs (
-                       id BIGSERIAL PRIMARY KEY, -- 블로그 ID (Primary Key)
-                       intro TEXT,               -- 블로그 소개글
-                       user_id BIGINT NOT NULL,  -- 사용자 ID (Foreign Key로 사용될 수 있음)
-                       create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 생성일 (자동으로 생성됨)
-                       modify_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  -- 수정일 (자동으로 업데이트됨)
+                       user_id BIGINT PRIMARY KEY,                -- 블로그 소유자의 회원 ID (Primary Key)
+                       intro VARCHAR(255),                        -- 블로그 소개글 (최대 255자)
+                       is_public BOOLEAN NOT NULL,                -- 블로그의 공개 여부 (true: 공개, false: 비공개)
+                       create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 생성일 (자동 생성)
+                       modify_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  -- 수정일 (수동 업데이트 필요)
 );
