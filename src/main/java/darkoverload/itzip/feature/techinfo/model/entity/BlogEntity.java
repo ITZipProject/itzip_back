e57511da9ca97132e40ed090c269d69aff484344
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-29 15:47:37 +0900

    ♻️  리팩토링 : BlogEntity 클래스의 userId 필드와 UserEntity 연관 관계 수정
    - BlogEntity에서 userId 필드를 회원 ID로 사용하며, UserEntity와 1:1 매핑을 적용하여 소유자 정보를 명확하게 관리.
    - 블로그 소유자 정보를 UserEntity와 연결하여 도메인 객체 변환 시 사용자 정보도 함께 반환되도록 수정.
    - BlogEntity와 Blog 도메인 객체 간의 일관성을 유지하며 가독성과 확장성을 고려한 구조 개선.
    
    Ref : #125

--- src/main/java/darkoverload/itzip/feature/techinfo/model/entity/BlogEntity.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/model/entity/BlogEntity.java
@@ -1,63 +1,65 @@
 package darkoverload.itzip.feature.techinfo.model.entity;
 
 import darkoverload.itzip.feature.techinfo.domain.Blog;
+import darkoverload.itzip.feature.user.entity.UserEntity;
 import darkoverload.itzip.global.entity.AuditingFields;
 
 import jakarta.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
  * PostgreSQL에 저장되는 블로그 정보를 나타내는 엔티티 클래스.
- * 이 클래스는 블로그 ID, 소개글, 소유자(회원 ID)를 포함하며,
- * {@link AuditingFields}를 상속받아 생성 및 수정 날짜를 자동으로 관리한다.
- * {@link #convertToDomain()} 메서드를 통해 Blog 도메인 객체로 변환할 수 있다.
+ * 블로그 ID(회원 ID와 동일), 소개글, 소유자(회원)를 포함하고,
+ * 생성 및 수정 날짜를 자동으로 관리한다.
+ * {@link #convertToDomain()} 메서드를 통해 Blog 도메인 객체로 변환함.
  */
 @Entity
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
 @Table(name = "blogs")
 public class BlogEntity extends AuditingFields {
 
     /**
-     * 블로그의 고유 식별자 (PostgreSQL ObjectId).
+     * 블로그의 고유 식별자.
+     * 회원 ID와 동일하게 사용됨. 블로그는 회원과 1:1 관계를 가짐.
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
+
+    /**
+     * 블로그 소유자와의 일대일 매핑 관계.
+     * {@link UserEntity}와 매핑되어 해당 블로그의 소유자를 나타냄.
+     */
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity userEntity;
 
     /**
      * 블로그 소개글.
+     * 블로그에 대한 간단한 설명을 담고 있음.
      */
     private String intro;
 
     /**
-     * 블로그 소유자의 ID.
-     */
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
-
-    /**
-     * 블로그의 공개 여부 (true: 공개, false: 비공개).
+     * 블로그의 공개 여부.
+     * 블로그가 공개 상태인지 비공개 상태인지를 나타냄. (true: 공개, false: 비공개)
      */
     @Column(name = "is_public")
     private Boolean isPublic;
 
     /**
-     * BlogEntity를 Blog 도메인 객체로 변환한다.
+     * BlogEntity를 Blog 도메인 객체로 변환함.
      *
      * @return Blog 도메인 객체
      */
     public Blog convertToDomain() {
         return Blog.builder()
-                .id(this.id)
+                .id(this.userId)
+                .user(userEntity.convertToDomain())
                 .intro(this.intro)
-                .userId(this.userId)
                 .isPublic(this.isPublic)
                 .build();
     }
