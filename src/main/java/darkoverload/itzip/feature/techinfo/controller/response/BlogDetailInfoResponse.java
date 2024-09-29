8d83b9ecc694b6a9179bca560e6d0c22ce1ed13c
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-29 15:45:40 +0900

    ♻️  리팩토링 : BlogDetailInfoResponse 클래스 구조 수정 및 필드명 변경
    - author 필드를 nickname으로 수정하여 소유자의 정보 일관성 유지.
    - profileImagePath 필드를 주석 해제하여 블로그 소유자의 프로필 이미지 경로를 포함하도록 변경.
    
    Ref : #131

--- src/main/java/darkoverload/itzip/feature/techinfo/controller/response/BlogDetailInfoResponse.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/response/BlogDetailInfoResponse.java
@@ -21,17 +21,17 @@ public class BlogDetailInfoResponse {
     @Schema(description = "블로그의 고유 ID", example = "1")
     private Long blogId;
 
-    @Schema(description = "블로그 소유자의 이름", example = "hyoseung")
-    private String author;
+    @Schema(description = "블로그 소유자의 프로필 이미지 경로", example = "")
+    private String profileImagePath;
+
+    @Schema(description = "블로그 소유자의 닉네임", example = "hyoseung")
+    private String nickname;
 
     @Schema(description = "블로그 소유자의 이메일 주소", example = "dev.hyoseung@gmail.com")
     private String email;
 
     @Schema(description = "블로그 소개글", example = "이 블로그는 최신 기술 정보를 공유합니다.")
     private String intro;
-
-    // @Schema(description = "블로그 소유자의 프로필 이미지 경로", example = "")
-    // private String profileImagePath;
 
     @Schema(description = "연도, 월, 주차별로 포스트 수를 제공하는 리스트 구조로, 연도 -> 월 -> 주차별 포스트 수를 포함합니다.")
     private List<YearlyPostDto> postCountByYear;
