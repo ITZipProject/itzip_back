680535fe2b26ed8fc0fb4fee8fc85223e09004c5
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-29 15:50:14 +0900

    ♻️  리팩토링 : BlogUpdateRequest에서 불필요한 blogId 필드 제거
    - BlogUpdateRequest 객체에서 blogId 필드를 제거하여, 블로그 업데이트 시 불필요한 데이터 요청을 줄이고 간소화.
    - 필수 항목이 아닌 경우, 소개글만 업데이트할 수 있도록 요청 객체를 단순화하여 API 요청 구조 개선.
    
    Ref : #131

--- src/main/java/darkoverload/itzip/feature/techinfo/controller/request/BlogUpdateRequest.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/request/BlogUpdateRequest.java
@@ -9,16 +9,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Schema(
-        description = "블로그 업데이트 요청 객체로, 블로그 ID와 새로운 소개글을 포함합니다."
+        description = "블로그 업데이트 요청 객체로, 새로운 소개글을 포함합니다."
 )
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BlogUpdateRequest {
-
-    @Schema(description = "업데이트할 블로그의 ID", example = "1")
-    @NotNull(message = "블로그 ID는 필수 입력 항목입니다.")
-    private Long blogId;
 
     @Schema(description = "업데이트된 블로그 소개글", example = "수정된 블로그 소개글입니다.")
     private String intro;
