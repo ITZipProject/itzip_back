f49f8af3422e61bfad8f970610194515a82d37e7
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-29 16:07:18 +0900

    ♻️ 테이블 수정: blogs 테이블 수정 및 외래 키 제약 조건 추가

--- src/main/resources/sql/techinfo/techinfo_blogs.sql
+++ src/main/resources/sql/techinfo/techinfo_blogs.sql
@@ -1,8 +1,10 @@
 -- 테이블 생성: blogs
-CREATE TABLE blogs (
-                       user_id BIGINT PRIMARY KEY,                -- 블로그 소유자의 회원 ID (Primary Key)
-                       intro VARCHAR(255),                        -- 블로그 소개글 (최대 255자)
-                       is_public BOOLEAN NOT NULL,                -- 블로그의 공개 여부 (true: 공개, false: 비공개)
-                       create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 생성일 (자동 생성)
-                       modify_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  -- 수정일 (수동 업데이트 필요)
+CREATE TABLE blogs
+(
+    user_id     BIGINT PRIMARY KEY,                                                       -- 블로그의 고유 식별자, 회원 ID와 동일하게 사용됨
+    intro       VARCHAR(255),                                                             -- 블로그 소개글 (최대 255자)
+    is_public   BOOLEAN   NOT NULL,                                                       -- 블로그 공개 여부 (true: 공개, false: 비공개)
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,                             -- 생성일 (자동 생성)
+    modify_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정일 (자동 업데이트)
+    CONSTRAINT fk_blog_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE -- 외래 키 제약 조건, 회원 ID를 참조함
 );
