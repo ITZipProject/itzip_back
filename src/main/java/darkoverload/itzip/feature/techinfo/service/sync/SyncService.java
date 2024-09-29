02f057c8952499ff832566e1839572d5cdc3cd35
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-29 20:35:26 +0900

    💩 똥싼 코드 : 패키지 경로 철자 오류 수정
    - 패키지 경로 오타 수정
    
    Ref : #129

--- src/main/java/darkoverload/itzip/feature/techinfo/service/sync/SyncService.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/service/sync/SyncService.java
@@ -1,7 +1,5 @@
 package darkoverload.itzip.feature.techinfo.service.sync;
 
-package darkoverload.itzip.feature.techinfo.service.sync;
-
 /**
  * 좋아요 상태를 Redis 캐시에서 MongoDB로 동기화하는 로직을 처리하는 서비스 인터페이스.
  * 주기적으로 Redis 캐시에 저장된 좋아요 상태를 조회하고 이를 MongoDB에 반영하는 메서드를 정의한다.
