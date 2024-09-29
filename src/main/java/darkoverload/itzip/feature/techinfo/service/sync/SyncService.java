b2117a4d4d93ee6009f7c3c16105af0c6bab80db
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-29 20:31:32 +0900

    💡 주석 추가 : Redis 캐시에서 MongoDB로 좋아요 상태 동기화에 대한 설명 추가
    - 좋아요 상태를 Redis 캐시에서 MongoDB로 동기화하는 로직 설명 주석 추가
    
    Ref : #129

--- src/main/java/darkoverload/itzip/feature/techinfo/service/sync/SyncService.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/service/sync/SyncService.java
@@ -1,6 +1,15 @@
 package darkoverload.itzip.feature.techinfo.service.sync;
 
+package darkoverload.itzip.feature.techinfo.service.sync;
+
+/**
+ * 좋아요 상태를 Redis 캐시에서 MongoDB로 동기화하는 로직을 처리하는 서비스 인터페이스.
+ * 주기적으로 Redis 캐시에 저장된 좋아요 상태를 조회하고 이를 MongoDB에 반영하는 메서드를 정의한다.
+ */
 public interface SyncService {
 
+    /**
+     * 주기적으로 호출되어 Redis 캐시에서 저장된 좋아요 상태를 MongoDB에 반영.
+     */
     void persistLikesToMongo();
 }
