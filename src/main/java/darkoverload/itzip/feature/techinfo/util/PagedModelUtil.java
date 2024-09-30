056dce4e7d869d5eae5bd727ace6035c67203a03
CodeJesterND <dev.hyoseung@gmail.com>
2024-09-30 14:45:42 +0900

    💡 주석 수정 : PagedModelUtil 클래스에 JavaDocs 추가
    - 유틸리티 클래스에 대한 설명 및 메서드 JavaDocs 작성
    
    - createPagedResponse 메서드의 기능과 파라미터 설명 추가
    
    - 클래스 인스턴스화 방지를 위한 private 생성자에 대한 설명 포함
    
    Ref : #141

--- src/main/java/darkoverload/itzip/feature/techinfo/util/PagedModelUtil.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/util/PagedModelUtil.java
@@ -7,6 +7,10 @@ import org.springframework.hateoas.PagedModel;
 
 import java.util.List;
 
+/**
+ * 페이징된 응답을 생성하기 위한 유틸리티 클래스.
+ * HATEOAS 기반의 PagedModel 객체를 생성합니다.
+ */
 public class PagedModelUtil {
 
     /**
@@ -16,6 +20,15 @@ public class PagedModelUtil {
         // 인스턴스화 방지: 유틸리티 클래스는 모든 메서드가 정적이므로 인스턴스화될 필요가 없음
     }
 
+    /**
+     * 리스트 응답을 기반으로 HATEOAS PagedModel 객체를 생성합니다.
+     *
+     * @param <T> 응답의 타입
+     * @param responses 응답 객체 리스트
+     * @param page 페이징 처리된 Page 객체
+     * @param pageable 페이지 정보
+     * @return HATEOAS 기반 PagedModel 객체
+     */
     public static <T> PagedModel<EntityModel<T>> createPagedResponse(List<T> responses, Page<T> page, Pageable pageable) {
 
         PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(
