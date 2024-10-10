f5defb47b6fbcd4abf9bce7bfdc2b6f138f49f38
CodeJesterND <dev.hyoseung@gmail.com>
2024-10-10 21:18:58 +0900

    💡 주석 수정 : 단어 명칭 명확히 수정

--- src/main/java/darkoverload/itzip/feature/techinfo/controller/blog/request/BlogUpdateRequest.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/blog/request/BlogUpdateRequest.java
@@ -7,13 +7,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Schema(
-        description = "블로그 업데이트 요청 객체로, 새로운 소개글을 포함합니다."
+        description = "블로그 수정 요청 객체로, 새로운 소개글을 포함합니다."
 )
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BlogUpdateRequest {
 
-    @Schema(description = "업데이트된 블로그 소개글", example = "수정된 블로그 소개글입니다.")
+    @Schema(description = "수정된 블로그 소개글", example = "수정된 블로그 소개글입니다.")
     private String intro;
 }
--- src/main/java/darkoverload/itzip/feature/techinfo/controller/blog/response/BlogAdjacentPostsResponse.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/blog/response/BlogAdjacentPostsResponse.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Schema(
-        description = "블로그에서 특정 포스트의 생성일을 기준으로 이전/다음에 위치한 인접한 포스트 목록을 포함하는 응답 객체"
+        description = "블로그에서 특정 게시글의 생성일을 기준으로 이전/다음에 위치한 인접한 게시글 목록을 포함하는 응답 객체"
 )
 @Getter
 @Builder
@@ -22,6 +22,6 @@ public class BlogAdjacentPostsResponse {
     @Schema(description = "블로그 회원의 닉네임", example = "hyoseung")
     private String nickname;  // 블로그 회원 닉네임
 
-    @Schema(description = "특정 포스트를 기준으로 생성일 순서에 따라 최신 및 이전 포스트를 포함한 인접한 포스트 목록을 제공합니다.")
-    private List<PostBasicResponse> posts;  // 생성일 기준 인접한 포스트 목록
+    @Schema(description = "특정 게시글를 기준으로 생성일 순서에 따라 최신 및 이전 게시글를 포함한 인접한 게시글 목록을 제공합니다.")
+    private List<PostBasicResponse> posts;  // 생성일 기준 인접한 게시글 목록
 }
