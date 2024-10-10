cac8fc1bdb3c4d6a36b38f1795aaf651c76914ab
CodeJesterND <dev.hyoseung@gmail.com>
2024-10-11 03:35:12 +0900

    :sparkles: 새 기능 : TechInfoPostPreviewController 구현 및 포스트 미리보기 기능 추가 - TechInfoPostPreviewController 클래스 추가하여 포스트 미리보기 API 구현
    - 포스트 미리보기 조회 엔드포인트 (/tech-info/posts/preview) 구현
      - 카테고리 ID(선택사항)와 정렬 기준에 따른 필터링 기능 제공
      - 페이지네이션 지원 (page, size 파라미터)
    
    - PostPreviewService를 활용하여 포스트 미리보기 조회 로직 처리
    
    - Swagger 어노테이션을 사용하여 API 문서화 및 설명 추가
    
    - 예외 처리를 위한 ExceptionCodeAnnotations 적용
      - NOT_FOUND_POSTS_FOR_CATEGORY, NOT_FOUND_POST 예외 처리
    
    - HATEOAS를 적용하여 PagedModel<EntityModel<PostPreviewResponse>> 형태로 응답 반환
    
    - Optional을 사용하여 선택적 카테고리 ID 파라미터 처리
    
    - SortType enum을 활용한 정렬 기준 지정 기능 구현
    
    Ref : #143

---
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/post/TechInfoPostPreviewController.java
@@ -0,0 +1,52 @@
+package darkoverload.itzip.feature.techinfo.controller.post;
+
+import darkoverload.itzip.feature.techinfo.controller.post.response.PostPreviewResponse;
+import darkoverload.itzip.feature.techinfo.service.post.PostPreviewService;
+import darkoverload.itzip.feature.techinfo.type.SortType;
+import darkoverload.itzip.global.config.response.code.CommonExceptionCode;
+import darkoverload.itzip.global.config.response.code.CommonResponseCode;
+import darkoverload.itzip.global.config.swagger.ExceptionCodeAnnotations;
+import darkoverload.itzip.global.config.swagger.ResponseCodeAnnotation;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+
+@Tag(
+        name = "Tech Info Posts Preview",
+        description = "포스트 미리보기 기능을 제공하는 API"
+)
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tech-info")
+public class TechInfoPostPreviewController {
+
+    private final PostPreviewService postPreviewService;
+
+    @Operation(
+            summary = "포스트 미리보기 조회",
+            description = "카테고리 ID 및 정렬 기준에 따라 필터링된 포스트 미리보기를 조회한다."
+    )
+    @GetMapping("/posts/preview")
+    @ResponseCodeAnnotation(CommonResponseCode.SUCCESS)
+    @ExceptionCodeAnnotations({
+            CommonExceptionCode.NOT_FOUND_POSTS_FOR_CATEGORY,
+            CommonExceptionCode.NOT_FOUND_POST
+    })
+    public PagedModel<EntityModel<PostPreviewResponse>> fetchFilteredPostPreviews (
+            @Parameter(description = "카테고리 ID (선택사항)", example = "66ce18d84cb7d0b29ce602f5") @RequestParam(name = "categoryId", required = false) Optional<String> categoryId,
+            @RequestParam(name = "sortType", required = false, defaultValue = "NEWEST") SortType sortType,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "12") int size)
+    {
+        return postPreviewService.getAllOrFilteredPosts(categoryId, sortType, page, size);
+    }
+}
