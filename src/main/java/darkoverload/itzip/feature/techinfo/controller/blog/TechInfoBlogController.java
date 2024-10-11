a8992bcfaef410f6e4b21dc21be02596dbb216f8
CodeJesterND <dev.hyoseung@gmail.com>
2024-10-11 15:59:15 +0900

    ♻️ 코드 리팩토링 : TechInfoBlogController의 API 경로 및 HTTP 메서드 수정 - 블로그 관련 API의 경로를 '/tech-info/blog'로 변경하여 일관성 확보
    - 블로그 기본 정보 조회 API 설명을 블로그 소개글과 소유자 이메일 반환으로 수정
    
    - 블로그 수정 API의 HTTP 메서드를 @PatchMapping으로 변경하여 수정 작업을 명확히 함
    
    Ref : #46

--- src/main/java/darkoverload/itzip/feature/techinfo/controller/blog/TechInfoBlogController.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/blog/TechInfoBlogController.java
@@ -31,18 +31,18 @@ import lombok.RequiredArgsConstructor;
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/tech-info")
+@RequestMapping("/tech-info/blog")
 public class TechInfoBlogController {
 
     private final BlogService blogService;
 
     @Operation(
             summary = "블로그 기본 정보 조회",
-            description = "지정된 블로그 ID를 사용해 블로그의 제목, 설명, 생성 날짜 등 기본 정보와 소유자의 닉네임을 반환한다."
+            description = "지정된 블로그 ID를 사용해 블로그 소개글과 소유자의 닉네임과 이메일을 반환한다."
     )
     @ResponseCodeAnnotation(CommonResponseCode.SUCCESS)
     @ExceptionCodeAnnotations(CommonExceptionCode.NOT_FOUND_BLOG)
-    @GetMapping("/blog/{blogId}/basic/")
+    @GetMapping("{blogId}/basic/")
     public BlogBasicInfoResponse retrieveBlogSummary(
             @Parameter(description = "블로그 ID", example = "1") @PathVariable @NotNull Long blogId)
     {
@@ -53,7 +53,7 @@ public class TechInfoBlogController {
             summary = "블로그 상세 정보 조회",
             description = "인증된 사용자의 이메일을 사용해 블로그의 상세 정보(포스트 목록, 카테고리 정보)와 월별 포스트 수를 조회한다."
     )
-    @GetMapping("/blog/{nickname}/details")
+    @GetMapping("/{nickname}/details")
     @ResponseCodeAnnotation(CommonResponseCode.SUCCESS)
     @ExceptionCodeAnnotations({
             CommonExceptionCode.NOT_FOUND_USER,
@@ -69,14 +69,14 @@ public class TechInfoBlogController {
 
     @Operation(
             summary = "블로그 수정",
-            description = "사용자가 요청한 내용을 기반으로 블로그 제목, 설명 등의 정보를 업데이트한다."
+            description = "사용자가 요청한 내용을 기반으로 블로그의 소개글을 수정한다."
     )
     @ResponseCodeAnnotation(CommonResponseCode.SUCCESS)
     @ExceptionCodeAnnotations({
             CommonExceptionCode.NOT_FOUND_USER,
             CommonExceptionCode.NOT_FOUND_BLOG
     })
-    @PostMapping("")
+    @PatchMapping("")
     public String editBlogDetails(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody @Valid BlogUpdateRequest request)
