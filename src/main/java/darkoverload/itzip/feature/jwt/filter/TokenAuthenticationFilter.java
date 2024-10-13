234067e5654158e9e22f0d649e7702daeb1bf0fc
김채민 <dktjdej25@gmail.com>
2024-10-13 22:09:49 +0900

    🛠️ 리팩토링 : 로그인 인증 시 jwt 가져오는 방식 쿠키 -> 헤더로 변경
    Related to: #157

--- src/main/java/darkoverload/itzip/feature/jwt/filter/TokenAuthenticationFilter.java
+++ src/main/java/darkoverload/itzip/feature/jwt/filter/TokenAuthenticationFilter.java
@@ -4,7 +4,6 @@ import darkoverload.itzip.feature.jwt.infrastructure.CustomUserDetails;
 import darkoverload.itzip.feature.jwt.infrastructure.JwtAuthenticationToken;
 import darkoverload.itzip.feature.jwt.util.JwtTokenizer;
 import darkoverload.itzip.feature.user.entity.Authority;
-import darkoverload.itzip.feature.user.util.CookieUtils;
 import darkoverload.itzip.global.config.response.code.CommonExceptionCode;
 import darkoverload.itzip.global.config.response.exception.RestApiException;
 import io.jsonwebtoken.Claims;
@@ -29,12 +28,14 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * JWT 토큰 인증 필터 클래스
+ * JWT 인증 필터 클래스
  */
 @Slf4j
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenizer jwtTokenizer;
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
 
     /**
      * 필터 메서드
@@ -49,7 +50,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 쿠키에 저장된 accessToken 값을 가져옴
-        String accessToken = CookieUtils.findCookieValue(request, "accessToken").orElse(null);
+        String accessToken = resolveToken(request);
 
         if (StringUtils.hasText(accessToken)) {
             try {
@@ -77,7 +78,6 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private void getAuthentication(String token) {
         Claims claims = jwtTokenizer.parseAccessToken(token); // 토큰에서 클레임을 파싱
         String email = claims.getSubject(); // 이메일을 가져옴
-        Long userId = claims.get("userId", Long.class); // 사용자 ID를 가져옴
         String nickname = claims.get("nickname", String.class); // 이름을 가져옴
         Authority authority = Authority.valueOf(claims.get("authority", String.class)); // 사용자 권한을 가져옴
 
@@ -86,5 +86,16 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         CustomUserDetails userDetails = new CustomUserDetails(email, "", nickname, (List<GrantedAuthority>) authorities);
         Authentication authentication = new JwtAuthenticationToken(authorities, userDetails, null); // 인증 객체 생성
         SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContextHolder에 인증 객체 설정
+    }
+
+    /**
+     * Request Header에서 토큰 정보를 꺼내오기 위한 resolveToken 메서드
+     */
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
