b42ad05de1d7561596cccc8ca1c1dea43d508f3a
CodeJesterND <dev.hyoseung@gmail.com>
2024-10-15 12:33:30 +0900

    ✨ 새 기능 : 블로그 생성 서비스 구현 - 사용자 정보를 기반으로 블로그를 생성하는 CreateBlogService 인터페이스와 구현체 CreateBlogServiceImpl 추가
    - Blog 객체를 생성하여 BlogRepository에 저장하는 로직 구현
    
    Ref : #160

---
+++ src/main/java/darkoverload/itzip/feature/techinfo/service/blog/create/CreateBlogServiceImpl.java
@@ -0,0 +1,27 @@
+package darkoverload.itzip.feature.techinfo.service.blog.create;
+
+import darkoverload.itzip.feature.techinfo.domain.Blog;
+import darkoverload.itzip.feature.techinfo.repository.blog.BlogRepository;
+import darkoverload.itzip.feature.user.domain.User;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CreateBlogServiceImpl implements CreateBlogService {
+
+    // 블로그 정보를 저장하고 관리하는 리포지토리
+    private final BlogRepository blogRepository;
+
+    @Override
+    public void createBlog(User user) {
+
+        // 사용자 정보를 기반으로 블로그 객체 생성
+        Blog blog = new Blog(user);
+
+        // 블로그 객체를 엔티티로 변환 후 저장
+        blogRepository.save(blog.convertToEntity());
+    }
+}
