5973c956c3b4aa6f09b7ceee759b0ea94a33c9ca
hanseounggyun <hanseu9839@gmail.com>
2024-12-11 11:05:14 +0900

    🚑 긴급 수정 : 이력서 테스트 코드 에러 수정

--- src/test/java/darkoverload/itzip/feature/resume/domain/myskill/MySkillTest.java
+++ src/test/java/darkoverload/itzip/feature/resume/domain/myskill/MySkillTest.java
@@ -13,9 +13,9 @@ class MySkillTest {
 
     @Test
     void 스킬_정적_메소드_테스트() {
-        MySkill mySkill = MySkill.update(new MySkillsDto("python", 1L),  new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, 1L, null));
+        MySkill mySkill = MySkill.update(new MySkillsDto("python", 1L),  new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, 1L, null, null));
 
-        assertThat(mySkill).isEqualTo(new MySkill(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, 1L, null), "python", 1L));
+        assertThat(mySkill).isEqualTo(new MySkill(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, 1L, null, null), "python", 1L));
 
     }
 
--- src/test/java/darkoverload/itzip/feature/resume/domain/resume/ResumeTest.java
+++ src/test/java/darkoverload/itzip/feature/resume/domain/resume/ResumeTest.java
@@ -16,18 +16,10 @@ class ResumeTest {
     void 이력서_create_정적_메서드_성공_테스트() {
         ResumeDto resumeDto = new ResumeDto("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, null);
 
-        Resume resume = Resume.create(resumeDto, 1L, 1L);
+        Resume resume = Resume.create(resumeDto, 1L);
 
 
-        assertThat(resume).isEqualTo(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, null, null));
-
-    }
-
-    @Test
-    void 이력서_create_정적_메서드_실패_테스트() {
-        ResumeDto resumeDto = new ResumeDto("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, null);
-
-        assertThatThrownBy(() -> Resume.create(resumeDto, 1L, 2L)).isInstanceOf(RestApiException.class);
+        assertThat(resume).isEqualTo(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, null, null, null));
 
     }
 
@@ -37,19 +29,19 @@ class ResumeTest {
 
         Resume resume = Resume.update(resumeDto, 1L, 1L);
 
-        assertThat(resume).isEqualTo(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, 1L, null));
+        assertThat(resume).isEqualTo(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", PublicOnOff.YES, List.of("https://itzip.com"), null, 1L, 1L, null, null));
     }
 
 
     @Test
     void searchResume_테스트_성공_코드() {
-        Resume resume = Resume.searchResume(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", null, List.of("https://itzip.com"), null, 1L, 1L, null), "신입");
-        assertThat(resume).isEqualTo(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", null, List.of("https://itzip.com"), null, 1L, 1L, "신입"));
+        Resume resume = Resume.searchResume(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", null, List.of("https://itzip.com"), null, 1L, 1L, null, null), "신입");
+        assertThat(resume).isEqualTo(new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", null, List.of("https://itzip.com"), null, 1L, 1L, "신입", null));
     }
 
     @Test
     void userIdEqualsCheck_테스트_성공_코드() {
-        Resume resume = new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", null, List.of("https://itzip.com"), null, 1L, 1L, null);
+        Resume resume = new Resume("itzip@gmail.com", "010-9955-0938", "잇집 park입니다.", "잇집 park 입니다. 저는 코딩이 좋아요.", null, List.of("https://itzip.com"), null, 1L, 1L, null, null);
 
         assertThatThrownBy(() -> resume.userIdEqualsCheck(2L)).isInstanceOf(RestApiException.class);
     }
