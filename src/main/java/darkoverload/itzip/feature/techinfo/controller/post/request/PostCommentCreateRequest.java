332e05760541370389dc4a417510c7f155e5e29c
CodeJesterND <dev.hyoseung@gmail.com>
2024-12-26 05:31:30 +0900

    ♻️ 코드 리팩토링 : 게시글 도메인 로직 테스트를 위한 빌더 패턴 적용

--- src/main/java/darkoverload/itzip/feature/techinfo/controller/post/request/PostCommentCreateRequest.java
+++ src/main/java/darkoverload/itzip/feature/techinfo/controller/post/request/PostCommentCreateRequest.java
@@ -2,8 +2,12 @@ package darkoverload.itzip.feature.techinfo.controller.post.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
-@Schema(description = "기술 정보 게시글 댓글 작성 요청")
+@Schema(
+        description = "기술 정보 게시글 댓글 작성 요청"
+)
+@Builder
 public record PostCommentCreateRequest(
         @Schema(description = "게시글 ID", example = "66e724e50000000000db4e53")
         @NotBlank(message = "게시글 ID는 필수입니다.")
