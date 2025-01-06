60d2668f881b60ed94bfbc12c725f3fb8b626a63
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-06 23:53:54 +0900

    🗃 데이터베이스 관련 수정 : `users` 테이블에 초기 사용자 데이터 삽입 - `users` 테이블에 초기 사용자 데이터 추가
    Ref : #132

--- src/main/resources/sql/techinfo/techinfo_blogs.sql
+++ src/main/resources/sql/techinfo/techinfo_blogs.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE blogs
 (
     id          BIGSERIAL PRIMARY KEY,
@@ -9,6 +8,16 @@ CREATE TABLE blogs
     modify_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+INSERT INTO users (id, create_date, modify_date, authority, email, nickname, password, image_url)
+VALUES (1,
+        '2024-08-26 01:00:56.653362',
+        '2024-08-26 01:00:56.653362',
+        'USER',
+        'dev.hyoseung@gmail.com',
+        'hyoseung',
+        '$2a$10$JV5r580j9yGtPwj8FsPpo.UQ.H6j25fLZG6H1iYbtX4rYOCsQUXVa',
+        'https://dy1vg9emkijkn.cloudfront.net/profile/19cc111f-c8f4-4d64-bd7a-129415e3ffa2.jpg');
 
 INSERT INTO blogs (user_id, intro, is_public, create_date, modify_date)
 VALUES (1, 'This is a sample blog introduction.', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
