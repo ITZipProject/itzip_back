cd79b21150862f701e77a6b8ce9a0d5ed956bda5
김채민 <dktjdej25@gmail.com>
2025-01-15 14:41:28 +0900

    ✨ 새 기능 : sns 로그인 유저 비밀번호 재설정 차단 기능
    Related to: #27

--- src/main/java/darkoverload/itzip/feature/user/service/UserServiceImpl.java
+++ src/main/java/darkoverload/itzip/feature/user/service/UserServiceImpl.java
@@ -266,8 +266,9 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 비밀번호 재설정 요청 메서드
+     *
      * @param passwordResetRequest 비밀번호 재설정 요청 dto
-     * @param request 요청 객체
+     * @param request              요청 객체
      */
     @Override
     public String requestPasswordReset(PasswordResetRequest passwordResetRequest, HttpServletRequest request) {
@@ -275,6 +276,15 @@ public class UserServiceImpl implements UserService {
 
         // 올바른 이메일인지 체크
         User user = getByEmail(email);
+
+        // sns 로그인 회원 체크
+        if (user.getSnsType() != null) {
+            switch (user.getSnsType()) {
+                case "google" -> throw new RestApiException(CommonExceptionCode.GOOGLE_LOGIN_USER);
+                case "github" -> throw new RestApiException(CommonExceptionCode.GITHUB_LOGIN_USER);
+            }
+        }
+
 
         String tempPassword = PasswordUtil.generatePassword();
 
@@ -298,8 +308,9 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 비밀번호 재설정 승인 메서드
+     *
      * @param response 응답 객체
-     * @param token 비밀번호 재설정 토큰
+     * @param token    비밀번호 재설정 토큰
      */
     @Override
     public void confirmPasswordReset(HttpServletResponse response, String token) {
--- src/main/java/darkoverload/itzip/global/config/response/code/CommonExceptionCode.java
+++ src/main/java/darkoverload/itzip/global/config/response/code/CommonExceptionCode.java
@@ -74,6 +74,10 @@ public enum CommonExceptionCode implements ResponseCode {
     NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "이메일과 비밀번호가 일치하지 않습니다."),
     // 유저를 찾을 수 없음
     NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다."),
+    // 비밀번호 재설정 시 구글 로그인 계정
+    GOOGLE_LOGIN_USER(HttpStatus.BAD_REQUEST, "구글 로그인으로 회원가입한 계정은 비밀번호 재설정이 불가합니다."),
+    // 비밀번호 재설정 시 깃허브 로그인 계정
+    GITHUB_LOGIN_USER(HttpStatus.BAD_REQUEST, "깃허브 로그인으로 회원가입한 계정은 비밀번호 재설정이 불가합니다."),
 
     /**
      * TechInfo - Blog Error
