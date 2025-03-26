98ade06b2049c100a61027474fe997e18987a0f5
rowing0328 <dev.hyoseung@gmail.com>
2025-03-26 15:59:22 +0900

    ✨ 새 기능 : 커스텀 및 기본 데이터 삽입/삭제를 위한 SQL 스크립트 추가 - custom-delete-* 및 custom-insert-* 스크립트: 블로그, 댓글, 좋아요, 스크랩 등 특정 데이터 조작 처리 - default-delete-all 및 default-insert-* 스크립트: 전체 데이터 초기화 및 세팅용

---
+++ src/test/resources/sql/techinfo/default-delete-all-data.sql
@@ -0,0 +1,10 @@
+
+DELETE FROM scraps WHERE id = 999;
+
+DELETE FROM likes WHERE id = 999;
+
+DELETE FROM comments WHERE id = 999;
+
+DELETE FROM blogs WHERE user_id = 999;
+
+DELETE FROM users WHERE id = 999;
