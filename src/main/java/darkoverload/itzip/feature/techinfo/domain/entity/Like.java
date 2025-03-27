56831e07a9708e4b4478fc491a35e80122e124f3
rowing0328 <dev.hyoseung@gmail.com>
2025-03-28 00:36:59 +0900

    ✨ 새 기능 : Like 엔티티 구현 및 기본 좋아요 기능 추가 - 사용자와의 다대일 관계를 통한 Like 엔티티 생성 - articleId와 user 필드의 유니크 제약 조건 적용으로 중복 좋아요 방지 - 생성일(createdAt) 자동 설정 및 정적 팩토리 메소드(create) 구현
    Ref : #213

---
+++ src/main/java/darkoverload/itzip/feature/techinfo/domain/entity/Like.java
@@ -0,0 +1,60 @@
+package darkoverload.itzip.feature.techinfo.domain.entity;
+
+import darkoverload.itzip.feature.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(
+        name = "likes",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "article_id"})
+        }
+)
+@EqualsAndHashCode
+@EntityListeners(AuditingEntityListener.class)
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "article_id", nullable = false)
+    private String articleId;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    protected Like() {
+    }
+
+    public Like(final UserEntity user, final String articleId) {
+        this.user = user;
+        this.articleId = articleId;
+    }
+
+    public Like(final long id, final UserEntity user, final String articleId, final LocalDateTime createdAt) {
+        this.id = id;
+        this.user = user;
+        this.articleId = articleId;
+        this.createdAt = createdAt;
+    }
+
+    public static Like create(final UserEntity user, final String articleId) {
+        return new Like(user, articleId);
+    }
+
+}
