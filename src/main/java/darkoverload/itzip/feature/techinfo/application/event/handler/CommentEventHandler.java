82f0f39ac3cbb4996e02ddd445869c484f961e67
rowing0328 <dev.hyoseung@gmail.com>
2025-03-28 16:33:35 +0900

    ✨ 새 기능 : CommentEventHandler 구현 및 아티클 숨김 이벤트 처리  - ArticleHiddenEvent 수신 시 비동기 방식으로 댓글 전체 비공개 처리 기능 호출  - 이벤트 내 아티클 ID를 hex 문자열로 변환 후 CommentCommandService.deleteByArticleId 메소드 실행
    Ref : #213

---
+++ src/main/java/darkoverload/itzip/feature/techinfo/application/event/handler/CommentEventHandler.java
@@ -0,0 +1,25 @@
+package darkoverload.itzip.feature.techinfo.application.event.handler;
+
+import darkoverload.itzip.feature.techinfo.application.event.payload.ArticleHiddenEvent;
+import darkoverload.itzip.feature.techinfo.application.service.command.CommentCommandService;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentEventHandler {
+
+    private final CommentCommandService commandService;
+
+    public CommentEventHandler(final CommentCommandService commandService) {
+        this.commandService = commandService;
+    }
+
+    @Async
+    @EventListener
+    public void handleArticleHidden(final ArticleHiddenEvent event) {
+        final String articleIdHex = event.articleId().toHexString();
+        commandService.deleteByArticleId(articleIdHex);
+    }
+
+}
